e9ab3176cfaa4d86ce3225046ce61b9d246e48e6
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2023-06-08 16:24:10 +0530

    Code restored local

--- frontend/src/components/Donation.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function donation() {
-  return <div>donation</div>;
-}
--- frontend/src/components/SearchNearby.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SearchNearby() {
-  return <div>SearchNearby</div>;
-}
